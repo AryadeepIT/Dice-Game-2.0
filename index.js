862b39a12a1869b616e2d6c2420856a2ff938de5
Aryadeep Chakraborty <AryadeepIT@gmail.com>
2022-12-30 16:42:25 +0700

    Emoji added

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var randomNumber1 = Math.floor(Math.random()*6) +1;
+var randomNumber1 = Math.floor(Math.random()*6)+1;  //1-6
+var randomDiceImage= "dice"+randomNumber1+".png"; //dice1.png - dice6.png
+var randomImageSource = "images/"+randomDiceImage; //images/dice1.png - images/dice6.png
 
-var randomNumber2 = Math.floor(Math.random()*6) +1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
 
-document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1+".png");
+var randomNumber2 = Math.floor(Math.random()*6)+1;  //1-6
+var randomImageSource2 = "images/dice"+ randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2+".png");
+//if player1 wins
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "⛳ Player 1 Wins!";
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins!";
-    
-   } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").textContent = "Player 2 Wins!";
-    
-   } else {
-   document.querySelector("h1").textContent = "Draw!";
-   }
+}
+
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! ⛳";
+
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw! ⏱";
+
+}
